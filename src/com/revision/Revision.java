e248ac7f3e8864adf14b984cc1ae8da8d6d165f5
vjpratap <vijayprsingh619@gmail.com>
2015-03-20 20:30:31 +0530

    Initial commit

---
+++ src/com/revision/Revision.java
@@ -0,0 +1,59 @@
+package com.revision;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Revision {
+	private Set<String> contentVowels(){
+		String[] vowels = {"a","e","i","o","u","A","E","I","O","U"};
+		Set<String> vowelsSet = new HashSet<String>();
+		for(String vowel : vowels)
+			vowelsSet.add(vowel);
+		return vowelsSet;
+	}
+
+	private int giveDecimal(int binary, int base){
+		int decimal = 0, power = 0;
+		while(binary != 0){
+			if(binary%2 == 1)
+				decimal += (int)Math.pow(base, power);
+			power++;
+			binary /= 10;
+		}
+		return decimal;
+	}
+
+	public String reverse(String newStr){
+		int length = newStr.length(),i;
+		String s = "";
+		for(i = length - 1; i >= 0; i--)
+			s += newStr.substring(i, i + 1);
+		return s;
+	}
+	public int fibonacci(int term){
+		return (term == 1) ? 0 : (term == 2) ? 1 : fibonacci(term - 1) + fibonacci(term - 2);
+	}
+	public int readBinary(int binary){
+		return giveDecimal(binary,2);
+	}
+	public int readBinary(String binary){
+		int intBinary = Integer.parseInt(binary);
+		return giveDecimal(intBinary,2);
+	}
+	public int readOctal(int octal){
+		return giveDecimal(octal, 8);
+	}
+	public int readOctal (String octal){
+		int intOctal = Integer.parseInt(octal);
+		return giveDecimal(intOctal, 8);
+	}
+	public int getVowelCount(String word){
+		int count = 0;
+		for (int i = 0;i < word.length(); i++) {
+			
+			if(contentVowels().contains(word.substring(i, i + 1)))
+				count++;
+		}
+		return count;		
+	}
+}
---
+++ test/com/revision/RevisionTest.java
@@ -0,0 +1,72 @@
+package com.revision;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+public class RevisionTest {
+	@Test
+	public void giveReverseText(){
+		Revision r = new Revision();
+		assertEquals("olleh",r.reverse("hello"));
+		assertEquals(".olleh .olleh", r.reverse("hello. hello."));
+	}
+	@Test
+	public void getFibonacci(){
+		Revision r = new Revision();
+		assertEquals(1 , r.fibonacci(3));
+		assertEquals(0 , r.fibonacci(1));
+		assertEquals(3 , r.fibonacci(5));
+	}
+	@Test 
+	public void readBinary_reads_numbers_as_binary(){
+		Revision r = new Revision();
+		assertEquals(0,r.readBinary(0));
+		assertEquals(1,r.readBinary(1));
+		assertEquals(2,r.readBinary(10));
+		assertEquals(9,r.readBinary(1001));
+		assertEquals(255,r.readBinary(11111111));
+	}
+	@Test 
+	public void readBinary_reads_text_as_binary(){
+		Revision r = new Revision();
+		assertEquals(0,r.readBinary("0"));
+		assertEquals(1,r.readBinary("1"));
+		assertEquals(2,r.readBinary("10"));
+		assertEquals(9,r.readBinary("1001"));
+		assertEquals(255,r.readBinary("11111111"));	
+	}
+	@Test
+	public void readOctal_reads_numbers_as_octal(){
+		Revision r = new Revision();
+		assertEquals(0,r.readOctal(0));
+		assertEquals(1,r.readOctal(1));
+		assertEquals(8,r.readOctal(10));
+		assertEquals(513,r.readOctal(1001));
+		assertEquals(299593,r.readOctal(1111111));
+	};
+	@Test
+	public void readBinary_reads_text_as_octal(){
+		Revision r = new Revision();
+		assertEquals(0,r.readOctal("0"));
+		assertEquals(1,r.readOctal("1"));
+		assertEquals(8,r.readOctal("10"));
+		assertEquals(513,r.readOctal("1001"));
+		assertEquals(299593,r.readOctal("1111111"));
+	};
+	@Test
+	public void getVowelCount_gives_the_count_of_vowels(){
+		Revision r = new Revision();
+		assertEquals(r.getVowelCount("morning"),2);
+	}
+}
+
+
+
+
+
+
+
+
+
